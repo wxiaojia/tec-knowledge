07df9e3ab765488d5975972aa69585028d73b2b7
wxiaojia <630993632@qq.com>
2024-07-21 22:33:01 +0800

    feat: 视频内容

--- docs/.vitepress/router/av.js
+++ docs/.vitepress/router/av.js
@@ -5,6 +5,10 @@ const av = [
       text: '音视频',
       items: [
         { text: '笔记', link: baseUrl+'index' },
+        { text: '音视频学习全局', link: baseUrl+'learning' },
+        { text: '(1)音视频基础', link: baseUrl+'1' },
+        { text: '(2.1)音视频编码PCM和ACC', link: baseUrl+'2' },
+        { text: '(2.2)视频编码', link: baseUrl+'3' },
       ]
     }
   ]
